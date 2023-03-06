cc3274255ef99cf054d4521189bc99f4d8331ccf
Maksym <saponexamq@gmail.com>
2023-03-06 21:01:33 +0100

    Added model windows functionality across the entire page, as well as a horizontal and vertical slider

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,5 +1,7 @@
 const modals = () => {
-	function bindModel(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+	let btnPressed = false;
+
+	function bindModel(triggerSelector, modalSelector, closeSelector, destroy = false) {
 		const trigger = document.querySelectorAll(triggerSelector),
 			modal = document.querySelector(modalSelector),
 			close = document.querySelector(closeSelector),
@@ -11,9 +13,16 @@ const modals = () => {
 				if(e.target) {
 					e.preventDefault();
 				}
+
+				btnPressed = true;
+
+				if (destroy) {
+					item.remove();
+				}
 				
 				windows.forEach( item => {
 					item.style.display = 'none';
+					item.classList.add('animated', 'fadeIn');
 				});
 
 				modal.style.display = 'block';
@@ -33,7 +42,7 @@ const modals = () => {
 		});
 
 		modal.addEventListener('click', (e) => {
-			if(e.target === modal && closeClickOverlay) {
+			if(e.target === modal) {
 				windows.forEach( item => {
 					item.style.display = 'none';
 				});
@@ -64,17 +73,38 @@ const modals = () => {
 
 	function showModalByTime(selector, time) {
 		setTimeout(function() {
-			document.querySelector(selector).style.display ='block';
-			document.body.style.overflow = 'hidden';
+			let display;
+
+			document.querySelectorAll('[data-modal]').forEach(item => {
+				if (getComputedStyle(item).display !== 'none') {
+					 display = 'block';
+				}
+			});
+
+			if (!display) {
+				document.querySelector(selector).style.display ='block';
+				document.body.style.overflow = 'hidden';
+				let scroll = calcScroll();
+				document.body.style.marginRight = `${scroll}px`;
+			}
+
 		}, time);
 	} 
+
+	function openByScroll(selector) {
+		window.addEventListener('scroll', () => {
+			if (!btnPressed && (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight)) {
+				document.querySelector(selector).click();
+			}
+		});
+	}
 	
-	bindModel('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
-	bindModel('.phone_link', '.popup', '.popup .popup_close');
-	showModalByTime('.popup', 60000);
-	bindModel('.popup_calc_btn', '.popup_calc', '.popup_calc_close')
-	bindModel('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false)
-	bindModel('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false)
+	bindModel('.button-design', '.popup-design', '.popup-design .popup-close');
+	bindModel('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
+	bindModel('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true);
+	openByScroll('.fixed-gift');
+
+	showModalByTime('.popup-consultation', 15000);
 }
 
 export default modals
